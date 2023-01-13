073f7ac3da7005de8b1865260aa2f4ad12c44bb8
koskv <koskv@list.ru>
2023-01-13 19:18:34 +0300

    v 1.0.1

--- src/test/java/api/ReqresTest.java
+++ src/test/java/api/ReqresTest.java
@@ -25,19 +25,33 @@ public class ReqresTest {
                 // извлекаем body и записываем блок data в созданный класс (в те переменные).
                 .extract().body().jsonPath().getList("data",UserData.class);
         //1. Выбираем все аватары и все id
-      //  users.stream().forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
+        users.stream().forEach(x-> Assert.assertTrue(x.getAvatar().contains(x.getId().toString())));
         //2. то же самое через цикл for
         /*       for (UserData x : users) {
             Assert.assertTrue(x.getAvatar().contains(x.getId().toString()));
         }*/
         //3. Два списка и сравниваем в цикле
-        List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
+     /*   List<String> avatars = users.stream().map(UserData::getAvatar).collect(Collectors.toList());
         List<String> ids = users.stream().map(x->x.getId().toString()).collect(Collectors.toList());
         for (int i=0; i < users.size(); i++){
-            Assert.assertTrue(avatars.get(i).contains(ids.get(i)));
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i))); */
+
         }
 
+    @Test
+    public void checkEmail() {
+        List<UserData> users = given()
+                .when()
+                .contentType(ContentType.JSON)
+                .get(URL+ "/api/users?page=2")
+                .then().log().all()
+                .extract().body().jsonPath().getList("data", UserData.class);
+        List<String> emails = users.stream().map(UserData::getEmail).collect(Collectors.toList());
+        for(int i=0;i<emails.size();i++){
+            Assert.assertTrue(emails.get(i).contains("@reqres.in"));
+        }
     }
+
 
 
 }
